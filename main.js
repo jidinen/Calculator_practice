4bdb6789b0c3754be1aee8a2264eebfbc22eae8b
jidinen <gofwenjidinen05@gmail.com>
2026-08-21 14:59:51 +0100

    Added the javascript file

---
+++ main.js
@@ -0,0 +1,16 @@
+let disp_int = document.querySelector(".button-funcx")
+
+let display_screen = document.querySelector(".display_screen")
+
+disp_int.addEventListener("click", ()=>{
+
+let textt = disp_int.textContent
+
+
+display_screen.value = textt
+
+
+
+
+
+})

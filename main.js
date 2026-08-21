f36b83f6d4838102b837a4e2b959ecbd5a29e293
jidinen <gofwenjidinen05@gmail.com>
2026-08-21 15:50:01 +0100

    Added the AC functionalty

--- main.js
+++ main.js
@@ -2,15 +2,46 @@ let disp_int = document.querySelector(".button-funcx")
 
 let display_screen = document.querySelector(".display_screen")
 
+let arr = []
+
 disp_int.addEventListener("click", ()=>{
 
 let textt = disp_int.textContent
 
 
-display_screen.value = textt
+
+arr.push(textt)
 
 
-
-
+ display_screen.value = arr.join('')
 
 })
+
+
+
+let all_cleared_btn = document.querySelector(".All_cleared")
+
+
+all_cleared_btn.addEventListener("click", ()=>{
+
+    if(display_screen.value != "null"){
+
+        display_screen.value = "0";
+    }
+    
+
+})
+
+
+
+
+let active_btn = document.getElementsByClassName("button-func");
+
+for (let i = 0; i < active_btn.length; i++) {
+  active_btn[i].addEventListener("click", function() {
+    let textx = this.textContent;
+    arr.push(textx);
+    display_screen.value = arr.join('');
+  });
+}
+
